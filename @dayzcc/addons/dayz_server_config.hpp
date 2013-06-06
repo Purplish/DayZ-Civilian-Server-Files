73ba39265d082e6cb4245ae41cf9d27d1060816d
Nick <ure890@aol.com>
2013-06-05 17:31:36 -0700

    New Server.pbo, updated to the latest beta patch

--- @dayzcc/addons/dayz_server_config.hpp
+++ @dayzcc/addons/dayz_server_config.hpp
@@ -5,17 +5,15 @@
 // Enable player model check
 //  CheckModel true  - Player model is checked when logging in and replaced with default if invalid
 //  CheckModel false - Model check is disabled
-#define CheckModel true
-
-
+#define CheckModel false
 // Enable custom inventory
 //  CheckCustInv true  - Player gets a custom loadout when specified in the database
 //  CheckCustInv false - Custom inventory is not enabled
-#define CheckCustInv false
+#define CheckCustInv true
 // Enable object publishing check
 //  CheckObject true  - Tents and objects are checked and only published to database if valid classes
 //  CheckObject false - Object check is disabled and they are always published
-#define CheckObject false
+#define CheckObject true
 
 // Enable kill messages
 //  KillMsgs true  - Kill messages are enabled and are logged down
@@ -30,12 +28,12 @@
 #define HitMsgs true
 //  HitMsgsIngame true  - Hit messages are shown ingame, requires them to be enabled
 //  HitMsgsIngame false - Hit messages are not shown ingame
-#define HitMsgsIngame true
+#define HitMsgsIngame false
 
 // Switch between old and new way of spawning
 //  OldSpawn true  - Wrecks, crashsites and care packages have there own sqf files and you can change the loot inside
 //  OldSpawn false - Everything is in "server_spawnWreck", wrecks randomly de-/respawn while the server is running
-#define OldSpawn false
+#define OldSpawn true
 
 // Spawn helicopter crashsites
 //  SpawnHelis true  - Helicopter crashsites are spawned
@@ -46,9 +44,9 @@
 //  SpawnWrecks true  - Custom wrecks are spawned
 //  SpawnWrecks false - No custom wrecks are spawned
 #define SpawnWrecks true
-#define SpawnWrecksCount 18
+#define SpawnWrecksCount 10
 // Spawn care packages
 //  SpawnCare true  - Care packages are spawned
 //  SpawnCare false - No care packages are spawned
 #define SpawnCare true
-#define SpawnCareCount 4
+#define SpawnCareCount 5
